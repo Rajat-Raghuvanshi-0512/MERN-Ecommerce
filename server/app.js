40f7eff971b83ba7d6f4cd125ae19a18966a4c9b
Rajat <rajat.karnal@gmail.com>
2022-12-19 01:54:38 +0530

    Removed proxy and homepage

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require("cors");
+// const cors = require("cors");
 
-corsOptions = {
-  origin: ["https://shop-buddy-frontend.vercel.app", "http://localhost:3000"],
-  credentials: true,
-};
+// corsOptions = {
+//   origin: ["https://shop-buddy-frontend.vercel.app", "http://localhost:3000"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //Config path
 if (process.env.NODE_ENV !== "PRODUCTION") {
